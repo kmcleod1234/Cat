ef7459c29e9af8b07eea71ede44dc5e24c8c0184
kmcleod1234 <50832973+kmcleod1234@users.noreply.github.com>
2021-11-08 15:23:43 -0500

    Add files via upload

---
+++ cat.php
@@ -0,0 +1,16 @@
+
+<?php
+/**
+ * @package Cat Typing
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Cat Typing
+Plugin URI: https://github.com/kmcleod1234
+Description: Adds a picture of a cat to the top of every Post that is tagged "cat", and adds the date of publication to the beginning of the Title of the Post.
+Author: Ken McLeod
+Version: 1.0.0
+Author URI: https://github.com/kmcleod1234
+*/
+echo "Today is " . date("m/d/Y") "- Cat Typing";
+echo "typing-cat-gif.gif" width="480" height="480"/>;
